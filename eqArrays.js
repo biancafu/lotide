795083997cbb51966d6b7dfa581f570f229ffd5b
Bianca Fu <0509biancafu@gmail.com>
2022-11-16 01:34:13 +0000

    W1D2 eqArray function

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  let i=0
+  while(i < arr1.length && i < arr2.length){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
