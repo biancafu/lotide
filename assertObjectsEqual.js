8dd47a040f81f212602258d78079de2f89ddd51f
Bianca Fu <0509biancafu@gmail.com>
2022-11-17 02:19:17 +0000

    W1D3 compare two objects and print corresponding message

---
+++ assertObjectsEqual.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1,arr2) {
+  let i=0
+  while(i < arr1.length || i < arr2.length){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+  for (const o in object1) {
+    if (Array.isArray(object1[o])){
+      if(!eqArrays(object1[o],object2[o])) return false;
+    } else if (object1[o] !== object2[o]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  const message = eqObjects(actual,expected)?`✅✅✅ Assertion Passed [${inspect(actual)}] === [${inspect(expected)}]` : `🛑🛑🛑 Assertion Failed [${inspect(actual)}] !== [${inspect(expected)}]`;
+  console.log(message);
+};
+
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' });
