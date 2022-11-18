51bc7fb346f367f612bb1d9f5d90b30fbc37f495
Bianca Fu <0509biancafu@gmail.com>
2022-11-18 03:49:31 +0000

    slice array until comeback function returns truthy

---
+++ takeUntil.js
@@ -0,0 +1,28 @@
+const takeUntil = function(array, callback) {
+  //slice the element until callback returns truthy
+  //for of loop
+  let i=0;
+  for (const element of array) {
+    if(callback(element)){
+      return array.slice(0,i)
+    }
+    i++;
+  }
+  return array;
+  //for each
+  //let i=array.length;
+  // array.forEach((element,index) => {
+  //   if(callback(element))i=index;
+  // })
+  // return array.slice(0,i);
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
