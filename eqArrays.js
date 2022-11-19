2f5d3b13730a739cdce8216303bf0f70367c2463
Bianca Fu <0509biancafu@gmail.com>
2022-11-19 22:19:15 +0000

    W1D5 stretch recursion solution

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,22 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const {assertEqual} = require("./assertEqual");
 const eqArrays = function(arr1,arr2) {
   
-  if(arr1.length !== arr2.length){
-    return false;
+  if(arr1.length !== arr2.length) return false;
+
+  //recursive solution
+  for (const i in arr1) {
+    const value1 = arr1[i];
+    const value2 = arr2[i];
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      eqArrays(value1,value2);
+      //if(!eqArrays(value1, value2)) return false;
+      continue;
+    } 
+    if (value1 !== value2) {
+      return false;
+    }
   }
+  return true;
 
   //my previous code
   // let i=0
@@ -22,16 +28,21 @@ const eqArrays = function(arr1,arr2) {
   // }
 
   //professional coding
-  for (const i in arr1) {
-    const value1 = arr1[i];
-    const value2 = arr2[i];
+  // for (const i in arr1) {
+  //   const value1 = arr1[i];
+  //   const value2 = arr2[i];
 
-    if (value1 !== value2) {
-      return false;
-    }
-  }
-  return true;
+  //   if (value1 !== value2) {
+  //     return false;
+  //   }
+  // }
+  // return true;
 };
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true) // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);// => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)// => false
 
 
 module.exports = eqArrays;
