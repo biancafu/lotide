46ef41baf4a6dd512512fde72795b18bc890ee7f
Bianca Fu <0509biancafu@gmail.com>
2022-11-21 23:08:29 +0000

    implement chai testing cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
-const {tail, assertEqual} = require('../tail');
+const {tail} = require('../tail');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements
+describe("#tail", ()=>{
+  it("return [Lighthouse, Labs] from [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("return [2,3] from [1,2,3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3]);
+  });
+});
