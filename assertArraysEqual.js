892e9cf659bd937a028bfdb22b10e14d76f2564a
Bianca Fu <0509biancafu@gmail.com>
2022-11-21 22:32:54 +0000

    refactoring with export/import methods

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,9 @@
 
-const eqArrays = require("./eqArrays");
+const {eqArrays} = require("./eqArrays");
 
 function assertArraysEqual(arr1,arr2){
   const message = eqArrays(arr1,arr2)?`✅✅✅ Assertion Passed [${arr1}] === [${arr2}]` : `🛑🛑🛑 Assertion Failed [${arr1}] !== [${arr2}]`;
   console.log(message);
 }
 
-module.exports = {assertArraysEqual}
+module.exports = assertArraysEqual;
